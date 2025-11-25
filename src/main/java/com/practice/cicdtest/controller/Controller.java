c7f22d54d980883c983d7419ec0b13311e2daf71
juneunbae <dmsqo1403@naver.com>
2025-11-25 15:40:31 +0900

    ci/cd 테스트

--- src/main/java/com/practice/cicdtest/controller/Controller.java
+++ src/main/java/com/practice/cicdtest/controller/Controller.java
@@ -1,4 +1,17 @@
 package com.practice.cicdtest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class Controller {
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
 }
